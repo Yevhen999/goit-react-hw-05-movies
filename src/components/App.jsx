e7daf57d641ecae08174cf0700c5cf6c4c917e1f
Yevhen <leshchenkoyevhen@gmail.com>
2022-12-17 20:45:50 -0500

    add api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
+import { getTrending } from '../services/api';
 export const App = () => {
+  getTrending();
+
   return (
     <div
       style={{
@@ -7,7 +10,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
---
+++ src/services/api.jsx
@@ -0,0 +1,10 @@
+// import axios from 'axios';
+
+const API_KEY = '9593e82df53f500ce20f9f064c8219d2';
+const baseUrl = 'https://api.themoviedb.org/3/';
+export const getTrending = async () => {
+  const response = await fetch(`${baseUrl}trending/all/day?api_key=${API_KEY}`);
+  const data = await response.json();
+  console.log(data.results);
+  return data;
+};
