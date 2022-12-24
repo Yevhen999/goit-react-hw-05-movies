9a533162491c4ee16dd60bf9807935f083762eb7
Yevhen <leshchenkoyevhen@gmail.com>
2022-12-24 14:12:17 -0500

    add location to cast and reviews

--- src/components/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/AdditionalInfo/AdditionalInfo.jsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './AdditionalInfo.module.css';
 
 const AdditionalInfo = () => {
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/movies';
+
   return (
     <div className={css.wrap}>
       <h2>Additional info</h2>
       <ul>
         <li>
-          <Link to="cast">Cast</Link>
+          <Link to="cast" state={{ from: backLinkHref }}>
+            Cast
+          </Link>
         </li>
         <li>
-          <Link to="reviews">Reviews</Link>
+          <Link to="reviews" state={{ from: backLinkHref }}>
+            Reviews
+          </Link>
         </li>
       </ul>
     </div>
